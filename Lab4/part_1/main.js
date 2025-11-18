8c8a9c0423145f45c851d32b7893a4d88b2b9943
vivan2110 <patelvivan2110@gmail.com>
2025-11-18 10:55:59 -0500

    commit 4
    I have correct 1 error in generateStory

--- Lab4/part_1/main.js
+++ Lab4/part_1/main.js
@@ -11,30 +11,30 @@ const generateBtn = document.querySelector(".generate");
 const story = document.querySelector(".story");
 
 function randomValueFromArray(array) {
-  const random = Math.floor(Math.random() * array.length);
-  return array[random];
+    const random = Math.floor(Math.random() * array.length);
+    return array[random];
 }
 
 // Raw text strings
 const characters = [
-  "Willy the Goblin",
-  "Big Daddy",
-  "Father Christmas"
+    "Willy the Goblin",
+    "Big Daddy",
+    "Father Christmas"
 ];
 
 const places = [
-  "the soup kitchen",
-  "Disneyland",
-  "the White House"
+    "the soup kitchen",
+    "Disneyland",
+    "the White House"
 ];
 // the soup kitchen
 // Disneyland
 // the White House
 
 const events = [
-  "spontaneously combusted",
-  "melted into a puddle on the sidewalk",
-  "turned into a slug and slithered away"
+    "spontaneously combusted",
+    "melted into a puddle on the sidewalk",
+    "turned into a slug and slithered away"
 ];
 // spontaneously combusted
 // melted into a puddle on the sidewalk
@@ -50,30 +50,30 @@ function returnRandomStoryString() {
     const storyText = `It was 94 Fahrenheit outside, so ${randomCharacter} went for a walk. When they got to ${randomPlace}, they stared in horror for a few moments, then ${randomEvent}. Bob saw the whole thing, but was not surprised — ${randomCharacter} weighs 300 pounds, and it was a hot day.`;
 
 
-  // 
-  return storyText;
+    // 
+    return storyText;
 }
 
 // Event listener and partial generate function definition
 
 generateBtn.addEventListener("click", generateStory);
 
-let storyText = returnRandomStoryString();
 function generateStory() {
-  if (customName.value !== "") {
-    const name = customName.value;
-    newStory = newStory.replace("Bob", name);
-  }
+    let newStory = returnRandomStoryString();
+    if (customName.value !== "") {
+        const name = customName.value;
+        newStory = newStory.replace("Bob", name);
+    }
 
-  // Convert to UK units
-  if (document.getElementById("uk").checked) {
-    const weight = Math.round(300 * 0.0714286) + " stone";
-    const temperature = Math.round((94 - 32) * 5 / 9) + " Celsius";
+    // Convert to UK units
+    if (document.getElementById("uk").checked) {
+        const weight = Math.round(300 * 0.0714286) + " stone";
+        const temperature = Math.round((94 - 32) * 5 / 9) + " Celsius";
 
-    newStory = newStory.replace("300 pounds", weight);
-    newStory = newStory.replace("94 Fahrenheit", temperature);
-  }
+        newStory = newStory.replace("300 pounds", weight);
+        newStory = newStory.replace("94 Fahrenheit", temperature);
+    }
 
-  story.textContent = newStory;
-  story.style.visibility = "visible";
+    story.textContent = newStory;
+    story.style.visibility = "visible";
 }
