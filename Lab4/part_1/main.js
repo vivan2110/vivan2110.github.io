5988cec50baac2e38b39d1047fcb8f8c6601f1db
vivan2110 <patelvivan2110@gmail.com>
2025-11-18 10:53:54 -0500

    commite-3
    according top requirements i added formula and also add new story line and also replace Bob with Name

--- Lab4/part_1/main.js
+++ Lab4/part_1/main.js
@@ -58,17 +58,22 @@ function returnRandomStoryString() {
 
 generateBtn.addEventListener("click", generateStory);
 
+let storyText = returnRandomStoryString();
 function generateStory() {
   if (customName.value !== "") {
     const name = customName.value;
+    newStory = newStory.replace("Bob", name);
   }
 
+  // Convert to UK units
   if (document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature = Math.round(94);
+    const weight = Math.round(300 * 0.0714286) + " stone";
+    const temperature = Math.round((94 - 32) * 5 / 9) + " Celsius";
+
+    newStory = newStory.replace("300 pounds", weight);
+    newStory = newStory.replace("94 Fahrenheit", temperature);
   }
 
-  // TODO: replace "" with the correct expression
-  story.textContent = "";
+  story.textContent = newStory;
   story.style.visibility = "visible";
 }
