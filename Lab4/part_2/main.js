56521ab37a199456355f961d6196ea8f758250d0
vivan2110 <patelvivan2110@gmail.com>
2025-11-18 11:05:42 -0500

    Commit_1_6
    I forgot to add css file in it

--- Lab4/part_2/main.js
+++ Lab4/part_2/main.js
@@ -4,3 +4,8 @@ File: main.js
 Date: 18 November 2025
 Description: JavaScript logic for Image Gallery
 */
+const displayedImage = document.querySelector(".displayed-img");
+const thumbBar = document.querySelector(".thumb-bar");
+
+const btn = document.querySelector("button");
+const overlay = document.querySelector(".overlay");
